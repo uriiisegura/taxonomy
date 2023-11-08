17a356308f31842b4a9f4053d7999a85452b4ad0
Oriol Segura Niño <okiko22@gmail.com>
2023-11-08 14:45:07 +0100

    Update data

--- src/components/Symbols.js
+++ src/components/Symbols.js
@@ -4,6 +4,8 @@ const SYMBOLS = {
 	'Phylum': <span className="level-symbol green"><span className="view">P</span><span className="full">Phylum</span></span>,
 	'Subphylum': <span className="level-symbol sub-green"><span className="view">sp</span><span className="full">subphylum</span></span>,
 	'Class': <span className="level-symbol blue"><span className="view">C</span><span className="full">Class</span></span>,
+	'Subclass': <span className="level-symbol sub-blue"><span className="view">sc</span><span className="full">subclass</span></span>,
+	'Infraclass': <span className="level-symbol infra-blue"><span className="view">ic</span><span className="full">infraclass</span></span>,
 	'Order': <span className="level-symbol purple"><span className="view">O</span><span className="full">Order</span></span>,
 	'Suborder': <span className="level-symbol sub-purple"><span className="view">so</span><span className="full">suborder</span></span>,
 	'Superfamily': <span className="level-symbol super-orange"><span className="view">SF</span><span className="full">Superfamily</span></span>,
